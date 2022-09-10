4d5ef9a111c4a27590844187eb7aa9fef6e34199
Scott Guertin <sguertin@gmail.com>
2022-09-10 19:21:32 -0400

    Change log factory to log provider

--- time_tracker/logging.py
+++ time_tracker/logging.py
@@ -7,38 +7,7 @@ from rich.logging import RichHandler
 from time_tracker.constants import LogLevel
 
 
-class ILoggingFactory(metaclass=ABCMeta):
-    @classmethod
-    def __subclasshook__(cls, subclass):
-        return (
-            (hasattr(subclass, "get_logger") and callable(subclass.get_logger))
-            and (hasattr(subclass, "update_level") and callable(subclass.update_level))
-            or NotImplemented
-        )
-
-    @abstractmethod
-    def get_logger(self, name: str) -> Logger:
-        """Retrieves a configured instance of the Logger class with the appropriate name
-
-        Args:
-            name (str): The name for the logger
-
-        Returns:
-            Logger: The configured logger
-        """
-        raise NotImplementedError()
-
-    @abstractmethod
-    def update_level(self, log_level: LogLevel) -> None:
-        """Updates the default log level for the factory
-
-        Args:
-            log_level (LogLevel): The new log level to be used as the default
-
-        """
-        raise NotImplementedError()
-
-class LoggingFactory(ILoggingFactory):
+class LoggingProvider:
     _log_level: LogLevel
 
     def __init__(self, log_level: LogLevel):
